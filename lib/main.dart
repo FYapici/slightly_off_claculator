84e3b2ae469ae7a2bdabb63789fc7bb3c35f56aa
FYapici <mfurkanyapici@gmail.com>
2023-05-14 20:59:21 +0300

    First fix

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hesap Makinesi',
+      title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
